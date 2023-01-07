7022cab4eaf6ce7cd70fe16a31c1021d036b15b3
Zelemhan Idrisov <zelkarnayn@gmail.com>
2023-01-07 20:14:19 +0300

    2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './components/HomePage';
 import Layot from './components/Layot';
-import AboutUs from './components/HomePage/About/AboutUs';
+// import AboutUs from './components/HomePage/About/AboutUs';
 
 import Shop from './components/Shop/Shop';
 import Admin from './components/Admin';
@@ -21,7 +21,7 @@ function App() {
        <Routes>
         <Route path='/' element={<Layot/>}>
           <Route index element={<HomePage/>}/>
-          <Route path='/about' element={<AboutUs/>}/>
+          {/* <Route path='/about' element={<AboutUs/>}/> */}
           <Route path='/news' element={<NewsGet/>}/>
           <Route path='/news/:id' element={<NewsFull/>}/>
           <Route path='/news-form/' element={<NewsForm/>}/>
